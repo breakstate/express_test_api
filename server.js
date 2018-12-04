52cdc25ecb2ef70c310fa5e5e73bcf9a27cef529
bmoodley <juganaught72@gmail.com>
2018-12-04 17:22:31 +0200

    finally figured out how to handle data object from db.any requestgit add -A

--- server.js
+++ server.js
@@ -1,45 +1,96 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+// server.js
 
+// BASE SETUP
+// ============================================================================
+
+// call the packages we need
+var express		= require('express');	// call express
+var app			= express();			// define our app using express
+var bodyParser	= require('body-parser');
+var pg			= require('pg');
+var pgp			= require('pg-promise')(/*options*/);
+
+const cn = {
+    host: 'ec2-54-247-119-167.eu-west-1.compute.amazonaws.com',
+    port: 5432,
+    database: 'd98er28m6a6qle',
+    user: 'wwezgigpimzyqa',
+    password: '51245111f5582aa18dc48a3c21f9f0dae2e773285428b7731ace341cbee8c867',
+	ssl: true
+};
+
+const db = pgp(cn);
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST request
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Server initialization
-app.listen(8000, () => {
-	console.log('Server Started!');
-});
+var port = process.env.PORT || 8080;	// set out port
 
-// Route '/api/cats' (GET)
-app.route('/api/cats').get((req, res) => {
-	res.send({
-		cats: [{ name: 'Lilly' }, { name: 'Lucy' }]
+// ROUTES FOR OUT API
+// ============================================================================
+var router = express.Router();			// get instance of the express Router
+
+	// middleware to use for all requests
+	router.use(function(req, res, next) {
+		// do logging
+		console.log('Something is happening');
+		next(); // make sure we go to the next routes and don't stop here
 	});
-	console.log('GET all cats');
-});
 
-// Roure '/api/cats/:name' (GET)
-app.route('/api/cats/:name').get((req, res) => {
-	const requestedCatName = req.params['name'];
-	res.send({ name: requestedCatName });
-	console.log('GET specific cat');
-});
+	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+	router.get('/', function(req, res) {
+		console.log('GET /');
+		res.json({ message: 'Hooray! Welcome to our API!' });
+	});
 
-// Route '/api/cats' (POST)
-app.route('/api/cats').post((req, res) => {
-	//res.send(201, req.body);
-	res.status(201).send(req.body);
-	console.log('POST add new cat');
-});
+	// more routes for our API will happen here
 
-// Route '/api/cats/:name' (PUT) update
-app.route('/api/cats/:name').put((req, res) => {
-	//res.send(200, req.body);	
-	res.status(200).send(req.body);
-	console.log('PUT change existing cat' + req.body);
-});
+	// on routes that end in /bears
+	router.route('/bears')
+		.post(function(req, res) {
+			console.log('POST /bears');
+			res.json({ message: 'POST to /bears successful' });
+		})
 
-// Route '/api/cats/:name' (DELETE)
-app.route('/api/cats/:name').delete((req, res) => {
-	res.sendStatus(204);
-	console.log('DELETE specific cat');
-});
+		.get(function(req, res) {
+			console.log('GET all bears');
+
+			db.any("select * from user_info"/*where first_name = 'Jerome'*/)
+				.then(data => {
+					console.log('DATA:', data[0].first_name); // print data
+				})
+				.catch(error => {
+					console.log('ERROR:', error); // print the error
+				})
+				.finally(db.end); // print the error
+
+			res.json({ message: 'GET to /bears successful' });
+		})
+		
+	// on routes that end in /bears/:bear_id
+	router.route('/bears/:bear_id')
+		.get(function(req, res) { 
+			console.log('GET bear by specific ID');
+			res.json({ message: 'GET to /bears/:bear_id successful' });
+		})
+
+		.put(function(req, res) {
+			console.log('PUT to update bear');
+			res.json({ message: 'PUT to /bears/:bear_id successful'});
+		})
+
+		.delete(function(req, res) {
+			console.log('DELETE to remove bear');
+			res.json({ message: 'DELETE to /bears/:bear_id successful' });
+		});
+
+// REGISTER OUR ROUTES -----------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// ============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
